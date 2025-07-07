b21e3b307e858a529e8812ad0c47718701f58fcf
kelvinjp1999 <87286523+kelvinjp1999@users.noreply.github.com>
2025-07-07 10:19:36 -0300

    Adicionando menu hamburguer e items de destaque

--- js/index.js
+++ js/index.js
@@ -1,6 +1,29 @@
-const toggleMenu = document.getElementById('menu-mobile');
+const hamburguer = document.querySelector('.hamburguer');
+const nav = document.querySelector('.botoes-header');
+const saleMessage = document.querySelector('#sale-message')
+const sale = document.querySelector('.sale')
 
-toggleMenu.addEventListener('click', () => {
-    toggleMenu.classList.toggle('ativo');
-    
+hamburguer.addEventListener('click', () => {
+    nav.classList.toggle('ativo');
+    hamburguer.classList.toggle('ativo');
 });
+
+function ChangeOffer(){
+    let i=0
+    const offers= ['Entregamos na sua casa apartir das 19:00','Entrega Gratis em Toda Regiao Leste de SJC','Outra frase que ainda nao pensei']
+    const collors = ['rgb(0,183, 255)', '#1F2937','#F56B00']
+
+    setInterval(()=>{
+        if(i == offers.length - 1){
+            i=0
+            saleMessage.innerHTML = offers[i]
+            sale.style.backgroundColor = collors[i]
+        } else {
+            i++
+            saleMessage.innerHTML = offers[i]
+            sale.style.backgroundColor = collors[i]
+        }
+    },4000)
+}
+
+ChangeOffer()
