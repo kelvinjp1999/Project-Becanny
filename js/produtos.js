2ab183fae2e394c408a43ed91f940a885dbb6fdf
moises.ferreira <moises00.msr@gmail.com>
2025-07-08 15:45:19 -0300

    implementação de videos no carrosel

---
+++ js/produtos.js
@@ -0,0 +1,151 @@
+const dogItens=[
+    {
+        id:1,
+        name:'Pedigree adulto carne e vegetais',
+        size:'15Kg',
+        price:'169,90',
+        img:'https://a-static.mlcdn.com.br/1500x1500/racao-pedigree-para-caes-adultos-de-porte-medio-e-grande-sabor-carne-e-vegetais-27kg/petz/10001310000029/10c7307eafd150b844169cd5dde6da55.jpeg'
+    },
+    {
+        id:2,
+        name:'Pedigree adulto raÃ§as pequenas',
+        size:'15Kg',
+        price:'169,90',
+        img:'https://a-static.mlcdn.com.br/1500x1500/racao-pedigree-para-caes-adultos-de-racas-pequenas-15-kg/mundoanimalsaovicente/23eae35c70c611ecbfed4201ac18503a/02679ac165fd05fed3ad19c21cac6419.jpeg'
+    },
+    {id:3,
+        name:'Biscrock Racas pequenas ',
+        size:'1Kg',
+        price:'30,00',
+        img:'https://a-static.mlcdn.com.br/800x560/petisco-biscrok-adulto-racas-pequenas-1-kg-pedigree/olistplus/opmrwr6jl8tb9700/dcbd204cbf8f4471cce22689dcbc6a87.jpeg'},
+
+    {id:4,
+      name:'Biscrock Junior',
+      size:'15Kg',
+      price:'169,90',
+      img:'https://a-static.mlcdn.com.br/800x560/pedigree-biscrok-junior-230-g/olistsp/osp1zbhnjyvazw3p/          e5e585eeccfebf29affbbd63455a9928.jpeg'},
+
+    {id:5,
+        name:'Biscrock Racas pequenas ',
+        size:'150g',
+        price:'7,90',
+        img:'https://a-static.mlcdn.com.br/800x560/biscoito-biscrok-caes-adultos-racas-pequenas-150g-bolacha-petisco/gatopretostore/p08962/8315c786019f7a399b7285284d6ff314.jpeg'},
+     
+    {id:6,
+         name:'Marrobone',
+        size:'500g',
+        price:'20,00',
+        img:'https://a-static.mlcdn.com.br/800x560/biscoito-para-cachorro-pedigree-marrobone-adulto-carne-500g/petmaniaaquarismoeatacado/76b8c530008b11eeb7844201ac185049/12d05804ca5974bda0b89865671fc1fa.jpeg'}           
+]
+
+const catItens=[
+    {id:7,
+        name:'Whiskas CArne adulto', 
+        size:'10Kg',
+        price:'169,90',
+        img:'https://a-static.mlcdn.com.br/800x560/racao-whiskas-carne-10kg-gatos-minassal-whiskas/mercadopetbebedouro/448/4a925fcd084e12a914adffe9f269239b.jpeg'
+    },
+    {id:8,
+        name:'Whiskas Carne castrado ', 
+        size:'10Kg',
+        price:'169,90',
+        img:'https://a-static.mlcdn.com.br/800x560/alimento-whiskas-para-gato-adulto-sabor-carne-castrado-10-1kg/maringapetshopnet/10241456916/cae821fc3817722fd55bfbcd230bde00.jpg'},
+
+
+    {id:9,
+      name:'kitekat',
+      size:'',
+      price:'2,30 unidade (5 und 10,00',
+      img:'https://a-static.mlcdn.com.br/800x560/racao-kitekat-mix-de-carnes-para-gatos-adultos-20-kg/petloveoficial/310993/d5a60ff263d14749defb2932777fbc94.jpeg'},
+      
+      
+    {id:10,
+     name:'Whiskas bola de pelo',
+     size:'40g',
+     price:'9,90',
+     img:'https://cobasi.vteximg.com.br/arquivos/ids/1042487-368-368/Petiscos-Anti-bola-de-pelo-Whiskas-40g.jpg?v=638753893779770000'},
+
+
+    {id:11,
+        name:'whiskas pelo saudavel ',
+        size:'40g',
+        price:'9,90',
+        img:'https://cobasi.vteximg.com.br/arquivos/ids/1042476-368-368/Petiscos-Pelo-saudavel-Whiskas-40g.jpg?v=638121725915630000'},
+
+
+    {id:12,
+      name:'whiskas bola de pelo ',
+      size:'80g',
+      price:'15,50',
+      img:'https://a-static.mlcdn.com.br/800x560/petisco-anti-bola-de-pelo-p-gatos-whiskas-80g/agropetstore/7896029021804/515f82bcbfc5db28068501bf5ec57769.jpeg'}
+]       
+    
+
+const variablesItens=[
+    {id:13,
+        name:'Dreamies carne',
+        size:'60g',
+        price:'6,30',
+        img:'https://cobasi.vteximg.com.br/arquivos/ids/1047484-368-368/petisco-dreamies-carne-para-gatos-adultos-frente.png?v=638753910217200000'},
+
+    {id:14,
+        name:'Dreamies carne ',
+        size:'80g',
+        price:'10,50',
+        img:'https://cobasi.vteximg.com.br/arquivos/ids/1047484-368-368/petisco-dreamies-carne-para-gatos-adultos-frente.png?v=638753910217200000'},
+
+
+    {id:15,
+     name:'Dreamies carne  ',
+     size:'150g',
+     price:'17,90',
+     img:'https://io.convertiez.com.br/m/superpaguemenos/shop/products/images/54921/large/petisco-gato-dreamies-150g-sc-carne_102417.png'},
+
+
+    {id:16,
+     name:'dentastix ',
+     size:'45g',
+     price:'8,90',
+     img:'https://tfdfn2.vtexassets.com/arquivos/ids/233856-1200-1200?v=638246139500970000&width=1200&height=1200&aspect=true'},
+
+
+    {id:17,
+     name:'dentastix ',
+     size:'77g',
+     price:'9,90',
+     img:'https://tfdfn2.vtexassets.com/arquivos/ids/233856-1200-1200?v=638246139500970000&width=1200&height=1200&aspect=true'},
+
+    {id:18,
+     name:'dentastix ',
+     size:'110g',
+     price:'14,90',
+     img:'https://tfdfn2.vtexassets.com/arquivos/ids/233856-1200-1200?v=638246139500970000&width=1200&height=1200&aspect=true'},
+     
+     
+    {id:19,
+    name:'dentastix ',
+    size:'180g',
+    price:'17,90',
+    img:'https://tfdfn2.vtexassets.com/arquivos/ids/233856-1200-1200?v=638246139500970000&width=1200&height=1200&aspect=true'},
+        
+        
+    {id:20,
+    name:'dentastix ',
+    size:'270g',
+    price:'25,50',
+    img:'https://tfdfn2.vtexassets.com/arquivos/ids/233856-1200-1200?v=638246139500970000&width=1200&height=1200&aspect=true'},  
+]
+
+
+//Funcao do menu mobile
+const btnMenu = document.getElementById('btn-menu')
+
+function showMenu(){
+    const nav=document.querySelector('.nav')
+
+    nav.classList.toggle('open')
+}
+
+btnMenu.addEventListener('click',showMenu)
+
+// Fim da Funcao do menu mobile
